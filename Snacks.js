44721d57b46900100a6a59503d571834c0a84c31
Marco Mechini <mechinimarco35@gmail.com>
2025-03-11 15:09:54 +0100

    fatti snack da 6 a 9, 10 wip

--- Snacks.js
+++ Snacks.js
@@ -49,3 +49,71 @@ function stampaOgniSecondo(messaggio) {
 
 // stampaOgniSecondo('Sono timer!')
 
+// Snack 6
+
+function creaContatoreAutomatico(n) {
+    let contatore = n;
+    return function () {
+        setInterval(() => {
+            console.log(contatore);
+            contatore++;
+        }, 1000)
+    }
+}
+
+// creaContatoreAutomatico(2)()
+
+// Snack 7 TO DO
+
+function eseguiEFerma(messaggio, tAvvio, tStop) {
+
+    let tempo = 0;
+    let interval = setInterval(() => {
+        console.log(messaggio);
+        tempo += tAvvio;
+        if ((tempo + tAvvio) > tStop) {
+            clearInterval(interval)
+        }
+    }, tAvvio)
+}
+// eseguiEFerma('Ciao timer', 1000, 9000)
+
+// Snack 8
+
+function contoAllaRovescia(n) {
+    let interval = setInterval(() => {
+        if (n == 0) {
+            console.log('Tempo scaduto!');
+            clearInterval(interval);
+        }
+        else {
+            console.log(n);
+            n--;
+        }
+    }, 1000)
+
+}
+
+// contoAllaRovescia(5)
+
+// Snack 9
+sequenzaOperazioni([
+    () => console.log('Operazione 1'),
+    () => console.log('Operazione 2'),
+    () => console.log('Operazione 3'),
+], 1000)
+
+function sequenzaOperazioni(arr, intervallo) {
+    let counter = 0;
+    let interval = setInterval(() => {
+        if (arr.length <= counter) {
+            clearInterval(interval)
+        } else {
+            arr[counter]();
+            counter++
+        }
+
+    }, intervallo)
+}
+
+// Snack 10
